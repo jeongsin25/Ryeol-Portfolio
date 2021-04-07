5d8b9dcc7bd4950c9315c631d222342a780ab318
jeongsin25 <jeongsin25@naver.com>
2021-04-08 04:02:58 +0900

    ver_1

---
+++ home.js
@@ -0,0 +1,4 @@
+$(window).on('load' , function(){
+    $('.home_main').css('opacity' , '100').css('top' , '40%');
+    $('.face').css('opacity' , '100').css('transition-delay' , '0.5s');
+})
---
+++ nav.js
@@ -0,0 +1,9 @@
+$('.submenu li').on('hover' , function(){
+    $('.submenu li').css('background' , 'red');
+})
+$('.submenu li:nth-child(1)').css('background' , 'rgba(243, 243, 243, 1)')
+$(document).on('scroll' , function(){
+    // if(scrollX){
+
+    // }
+})
