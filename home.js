8971686b0df5ff59e247f3a60fe29a0098b60f77
jeongsin25 <jeongsin25@naver.com>
2021-04-14 04:59:11 +0900

    ver22

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 $(window).on('load' , function(){
-    $('.home_main').css('opacity' , '100').css('top' , '40%');
+    $('.home_main').css('opacity' , '100').css('top' , '35%');
     $('.face').css('opacity' , '100').css('transition-delay' , '0.5s');
 })
