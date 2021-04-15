9279dfc851fe80ef225973142592e6fcaac01fb6
jeongsin25 <jeongsin25@naver.com>
2021-04-16 06:26:45 +0900

    ver29

--- about.js
+++ about.js
@@ -1,11 +1,8 @@
 $(window).on('scroll' , function(){
-    if(window.scrollY > 350){
+    if(window.scrollY > window.innerHeight/2 - 150){
         $('.about_main').css('opacity' , '100').css('top' , '0')
         $('.face_img img').css('opacity' , '100');
         $('.list_total h1').css('opacity' , '100').css('top' , '0px');
         $('.list_total p b , .list_total p span').css('opacity' , '100').css('left' , '0');
-    }
-    else if(window.scrollY > 150){
-        $('.about_main').css('opacity' , '100').css('top' , '0')
     }
 })
--- skill.js
+++ skill.js
@@ -1,9 +1,6 @@
 $(window).on('scroll' , function(){
-    if(window.scrollY > 1250){
+    if(window.scrollY > window.innerHeight/2 + about.getBoundingClientRect().height - 150){
         $('.skill_main').css('opacity' , '100').css('top' , '0px');
         $('.skill_list li').css('opacity' , '100').css('top' , '0px');
-    }
-    else if(window.scrollY > 1050){
-        $('.skill_main').css('opacity' , '100').css('top' , '0px');
     }
 })
