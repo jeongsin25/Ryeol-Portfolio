9763e9d7743877b4a5f81adbb31127c0e586d47b
jeongsin25 <jeongsin25@naver.com>
2021-04-18 06:46:23 +0900

    ver40

--- menu.js
+++ menu.js
@@ -4,5 +4,34 @@ $(".menubox").on('click' , function(){
     }
     else if($("#menubox_1").is(":checked") == false){
         $(".menubar").css("top" , "-250px");
+        $("#menubox_1").prop("checked", false);
     }
 })
+
+$(".menubar ul li:nth-child(2)").on('click' , function(){
+    window.scroll(0,0);
+    $(".menubar").css("top" , "-250px");
+    $("#menubox_1").prop("checked", false)
+})
+$(".menubar ul li:nth-child(3)").on('click' , function(){
+    window.scroll(0,window.innerHeight);
+    $(".menubar").css("top" , "-250px");
+    $("#menubox_1").prop("checked", false)
+})
+$(".menubar ul li:nth-child(4)").on('click' , function(){
+    window.scroll(0,window.innerHeight + about.getBoundingClientRect().height);
+    $(".menubar").css("top" , "-250px");
+    $("#menubox_1").prop("checked", false)
+})
+$(".menubar ul li:nth-child(5)").on('click' , function(){
+    window.scroll(0,window.innerHeight + about.getBoundingClientRect().height + skill.getBoundingClientRect().height);
+    $(".menubar").css("top" , "-250px");
+    $("#menubox_1").prop("checked", false)
+})
+
+
+
+
+
+
+
