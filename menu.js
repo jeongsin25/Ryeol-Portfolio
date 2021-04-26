a0dd249de7573b417c59c35faa446d447212ae00
jeongsin25 <jeongsin25@naver.com>
2021-04-26 16:37:19 +0900

    ver45

--- menu.js
+++ menu.js
@@ -1,31 +1,31 @@
 $(".menubox").on('click' , function(){
     if($("#menubox_1").is(":checked") == true){
-        $(".menubar").css("top" , "0px");
+        $(".menubar").css("top" , "60px");
     }
     else if($("#menubox_1").is(":checked") == false){
-        $(".menubar").css("top" , "-250px");
+        $(".menubar").css("top" , "-190px");
         $("#menubox_1").prop("checked", false);
     }
 })
 
-$(".menubar ul li:nth-child(2)").on('click' , function(){
+$(".menubar ul li:nth-child(1)").on('click' , function(){
     window.scroll(0,0);
-    $(".menubar").css("top" , "-250px");
+    $(".menubar").css("top" , "-190px");
+    $("#menubox_1").prop("checked", false)
+})
+$(".menubar ul li:nth-child(2)").on('click' , function(){
+    window.scroll(0,window.innerHeight);
+    $(".menubar").css("top" , "-190px");
     $("#menubox_1").prop("checked", false)
 })
 $(".menubar ul li:nth-child(3)").on('click' , function(){
-    window.scroll(0,window.innerHeight);
-    $(".menubar").css("top" , "-250px");
+    window.scroll(0,window.innerHeight + about.getBoundingClientRect().height);
+    $(".menubar").css("top" , "-190px");
     $("#menubox_1").prop("checked", false)
 })
 $(".menubar ul li:nth-child(4)").on('click' , function(){
-    window.scroll(0,window.innerHeight + about.getBoundingClientRect().height);
-    $(".menubar").css("top" , "-250px");
-    $("#menubox_1").prop("checked", false)
-})
-$(".menubar ul li:nth-child(5)").on('click' , function(){
     window.scroll(0,window.innerHeight + about.getBoundingClientRect().height + skill.getBoundingClientRect().height);
-    $(".menubar").css("top" , "-250px");
+    $(".menubar").css("top" , "-190px");
     $("#menubox_1").prop("checked", false)
 })
 
